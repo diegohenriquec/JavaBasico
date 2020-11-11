bfaebfcfdfe130e695c073b4a9e56becd7928822
Diego Henrique Carmo do Nascimento Silva <sit_disilva@uolinc.com>
2020-11-11 18:06:04 -0300

    aula2

--- src/br/com/aulajava/CalcularSalario.java
+++ src/br/com/aulajava/CalcularSalario.java
@@ -3,19 +3,21 @@ package br.com.aulajava;
 public class CalcularSalario {
 
     public static void main(String[] args) {
-        //Informem 2 valores do tipo float.
-        //1 valor é o salario base
-        //2 valor é o total das vendas
-        //Apresentar valor do salario final + comissão que representa 10% do valor das vendas.
-
-        long valor1 = 2.5;
+        // Informem 2 valores do tipo float.
+        // 1 valor é o salario base
+        // 2 valor é o total das vendas
+        // Apresentar valor do salario final + comissão que representa 10% do valor das
+        // vendas.
+        double valor1 = 100.5;
         // valor do salario base
-        long valor2 = 10.5;
+        double valor2 = 9000.5;
         // valor total das vendas
-        long soma = valor1 + valor 2
+        double percentual = 10.0 / 9000.5;
+        // percentual 10% de 88.5
+        double resultado = valor1 + (percentual);
+        //resultado da soma entre salario base e total de vendas
+        System.out.println(resultado);
+        //Mostrar resultado
 
-
-
-        
     }
 }
